a574af7a960819aee2c94c06119266b6c7787799
ChristophB <chrib@hotmail.de>
2018-11-16 13:42:22 +0100

    implemented namespacehandling of host address in settings

--- renderer-process/settings.js
+++ renderer-process/settings.js
@@ -8,7 +8,11 @@ submitButton.addEventListener('click', (event) => {
 	var host = hostInput.value
 
 	if (host) {
+		if (!host.match(/^https?:\/\/.*/)) host = 'http://' + host
+
 		settings.set('host', host)
+
+		hostInput.value = host
 		showMessage('Settings updated!', 'success')
 	}
 })
